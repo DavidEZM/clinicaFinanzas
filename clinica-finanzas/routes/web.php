6a8c7553d6a48eb35e16d2aba8d4b7de2d5e4412
David Zuniga <davidexequias1998@gmail.com>
2024-08-18 22:00:51 -0400

    Actualizacion de web.php
    - Se agregaron las rutas para cada Views, y con el metodo class se llaman todas las funtiones, CRUD

--- clinica-finanzas/routes/web.php
+++ clinica-finanzas/routes/web.php
@@ -2,6 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PacienteController;
+use App\Http\Controllers\ProfesionalController;
+use App\Http\Controllers\TipoEspecialidadController;
+use App\Http\Controllers\ReservaController;
+use App\Http\Controllers\AtencionController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rutas para Pacientes
+Route::resource('pacientes', PacienteController::class);
+
+// Rutas para Profesionales
+Route::resource('profesionales', ProfesionalController::class);
+
+// Rutas para TipoEspecialidades
+Route::resource('tipoespecialidades', TipoEspecialidadController::class);
+
+// Rutas para Reservas
+Route::resource('reservas', ReservaController::class);
+
+// Rutas para Atenciones
+Route::resource('atenciones', AtencionController::class);
+
