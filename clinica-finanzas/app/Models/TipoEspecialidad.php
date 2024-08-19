3660adacf610d116e43ce632f099854cdaf0d487
David Zuniga <davidexequias1998@gmail.com>
2024-08-18 20:30:27 -0400

    Creacion TipoEspecialidades.php y modificacion de nombre variable

---
+++ clinica-finanzas/app/Models/TipoEspecialidad.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TipoEspecialidad extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nombre_especialidad'];
+
+    public function profesionales()
+    {
+        return $this->hasMany(Profesional::class);
+    }
+}
